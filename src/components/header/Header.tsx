d8df7e169018caf6e9f2b4785ff989246cb30ff3
Quentin Watt <watt.quentin@gmail.com>
2023-10-08 11:00:08 +0200

    close the menu on route change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import MenuIcon from "../icons/MenuIcon";
 import CartIcon from "../icons/CartIcon";
 import { useAppDispatch } from "../../store/hooks";
 import { openMenu } from "../../store/menuSlice/menu";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -10,7 +11,9 @@ const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center px-5 py-3 border-b">
       <MenuIcon onClick={() => dispatch(openMenu())} />
-      <span className="font-bold text-2xl">CC Jitters</span>
+      <Link to="/" className="font-bold text-2xl">
+        CC Jitters
+      </Link>
       <CartIcon onClick={() => {}} />
     </header>
   );
